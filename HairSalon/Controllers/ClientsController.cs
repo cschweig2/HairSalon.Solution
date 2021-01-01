768a8b4a5d03298e0848f82db773b97acbbfbce5
Chelsea Becker <cschweig2@gmail.com>
2020-12-31 17:37:09 -0800

    add route to index and display clients

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly HairSalonContext _db;
+
+        public ClientsController(HairSalonContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Client> model = _db.Clients.Include(clients => clients.FirstName).ToList();
+            return View(model);
+        }
+    }
+}
